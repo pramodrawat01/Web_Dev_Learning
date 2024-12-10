78cd0c171e5df9fad8066d70d00962dddc345d2d
PramodRawat <rawatpramod392@gmail.com>
2024-12-10 22:48:23 +0530

    current location access on weather app

--- Projects/WeatherApp/app.js
+++ Projects/WeatherApp/app.js
@@ -1,16 +1,16 @@
 console.log("hello ji ");
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 
-// Weather API call
+//Weather API call
 async function customWeather(){
     try{
-       let latitude = 28.7041;
-       let longitude = 77.1025;
+       let latitude = 28.6066259;
+       let longitude = 77.0601443;
        
        //fecth api call
-       const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
-       // convert to json file
-       const weatherData = await data.json();
+       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+       // convert to json file  
+       const weatherData = await response.json();
        let locationName = weatherData.name;
 
        console.log("Weather data of current location : ", weatherData);
@@ -27,7 +27,8 @@ async function onUI(data, name){
     weatherinput.textContent = `Temperature : ${data?.main?.temp.toFixed(2)} °C`
     let location = document.createElement('p'); 
     location.textContent = `Location : ${name}`
-
+    
     document.body.appendChild(location);
     document.body.appendChild(weatherinput);
 }
+customWeather();
