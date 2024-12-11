d572eead649fd1d09d8e393308df7d9175c9e709
PramodRawat <rawatpramod392@gmail.com>
2024-12-11 23:11:01 +0530

    weather API call

--- Projects/WeatherApp/app.js
+++ Projects/WeatherApp/app.js
@@ -1,34 +1,14 @@
-console.log("hello ji ");
+const userTab = document.querySelector("data-userWeather");
+const searchTab = document.querySelector("data-searchWeather"); 
+
+const userContainer = document.querySelector("weather-container");
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchContainer = document.querySelector("data-searchForm");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".show-weather-container");
+
+// variables neede
+
+let currentTab = userTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
-
-//Weather API call
-async function customWeather(){
-    try{
-       let latitude = 28.6066259;
-       let longitude = 77.0601443;
-       
-       //fecth api call
-       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
-       // convert to json file  
-       const weatherData = await response.json();
-       let locationName = weatherData.name;
-
-       console.log("Weather data of current location : ", weatherData);
-
-       onUI(weatherData, locationName)
-    }
-    catch(err){
-        console.log("the error you got here : ", err);
-    }
-}
-
-async function onUI(data, name){
-    let weatherinput = document.createElement('p');
-    weatherinput.textContent = `Temperature : ${data?.main?.temp.toFixed(2)} °C`
-    let location = document.createElement('p'); 
-    location.textContent = `Location : ${name}`
-    
-    document.body.appendChild(location);
-    document.body.appendChild(weatherinput);
-}
-customWeather();
+currentTab.classList.add("current-tab");
