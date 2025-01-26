3ea9aad13188c8876b3eb61838639783fc56c6b4
PramodRawat <rawatpramod392@gmail.com>
2025-01-26 18:47:41 +0530

    started react

--- Projects/Product_list_cart/app.js
+++ Projects/Product_list_cart/app.js
@@ -10,12 +10,14 @@
 
 
 document.querySelector('.cart-btn').addEventListener('click', ()=>{
-    document.querySelector('.quentity').classList.add("index")
-    document.querySelector('.cart-btn').classList.remove("index")
+    // document.querySelector('.quentity').classList.add("index")
+    // document.querySelector('.cart-btn').classList.add("index")
+    console.log(document.querySelector('.cart-btn'))
 })
-
-document.querySelector('.minus-icon').addEventListener('click',()=>{
-    document.querySelector('.minus-icon').classList.remove("index")
-    document.querySelector('.plus-icon').classList.add("index")
-
-})  
+document.querySelector('.cart-btn').addEventListener('click',()=>{
+    document.querySelector('.cart-btn').classList.add('hide')
+})
+// document.querySelector('.minus-icon').addEventListener('click',()=>{
+//     document.querySelector('.minus-icon').classList.remove("index")
+//     document.querySelector('.plus-icon').classList.add("index")
+// })  
---
+++ React/my-react-tailwind-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>Hello ji kaise ho</div>
+    </>
+  )
+}
+
+export default App
