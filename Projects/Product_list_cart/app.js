a7911f9ae4126a595b9e28d2ec4b9924552af4d3
PramodRawat <rawatpramod392@gmail.com>
2025-01-27 22:29:01 +0530

    add counter function for items

--- Projects/Product_list_cart/app.js
+++ Projects/Product_list_cart/app.js
@@ -1,23 +1,68 @@
 
 
-
-// document.querySelector('.cart-btn').addEventListener('click', ()=>{
-//     document.querySelector('.quentity').classList.add("index")  
-//     document.querySelector('.cart-btn').classList.remove("index")
-// })
-
-// const cartbtnEle = document.querySelector('.cart-btn')
+const addCart = document.querySelector('.add-to-cart')
+const counter = document.querySelector('.counter')
 
 
-document.querySelector('.cart-btn').addEventListener('click', ()=>{
-    // document.querySelector('.quentity').classList.add("index")
-    // document.querySelector('.cart-btn').classList.add("index")
-    console.log(document.querySelector('.cart-btn'))
+
+addCart.addEventListener('click',()=>{
+    addCart.classList.add('hide')
 })
-document.querySelector('.cart-btn').addEventListener('click',()=>{
-    document.querySelector('.cart-btn').classList.add('hide')
+
+const incre = document.querySelector('.increment')
+const decre = document.querySelector('.decrement')
+const value = document.querySelector('.value')
+
+const totalItems = document.querySelector('.cart-item-count')
+
+// increment function
+const increment = ()=>{
+    let quentity = parseInt(value.innerText);
+    console.log(quentity)
+    quentity += 1;
+    value.innerText = quentity
+
+    let total = parseInt(totalItems.innerText);
+    total +=1;
+    totalItems.innerText = total
+}
+// decrement function
+const decrement = ()=>{
+    let quentity = parseInt(value.innerText);
+    console.log(quentity)
+    if(quentity>0){
+        quentity -= 1;
+    }
+    value.innerText = quentity
+
+    let total = parseInt(totalItems.innerText);
+    if(total>0){
+        total -=1;
+    }
+    totalItems.innerText = total
+}
+incre.addEventListener('click',increment)
+decre.addEventListener('click',decrement)
+
+let cart = []
+
+
+
+incre.addEventListener('click',()=>{
+    document.querySelector('.empty-now').classList.add('hide')
+
+    const nameText = document.querySelector('.dish-name')
+    const cart = document.querySelector('.cart')
+
+    // check item already exsit or not
+    let existingItems = Array.from(cart.children).find((items)=>{items.dataset.name === nameText})
+
+    if(existingItems){
+
+    }
+    else{
+        const name = nameText.innerText
+        cart.appendChild(name)
+
+    }
 })
-// document.querySelector('.minus-icon').addEventListener('click',()=>{
-//     document.querySelector('.minus-icon').classList.remove("index")
-//     document.querySelector('.plus-icon').classList.add("index")
-// })  
